a7dd2b578b8c041d9419e6df48d6887b767b9fd8
Axel Giottonini <axel.giottonini@gmail.com>
2022-12-14 23:40:02 +0100

    TE Dynamics - Phylogenetic

--- src/te_dynamics/main.sh
+++ src/te_dynamics/main.sh
@@ -17,6 +17,11 @@ UPDATE_FLYE=$(sbatch ./src/te_dynamics/update_gff.sh flye)
 FASTA_CANU=$(sbatch --dependency=afterok:${UPDATE_CANU##* } ./src/te_dynamics/get_fasta.sh canu)
 FASTA_FLYE=$(sbatch --dependency=afterok:${UPDATE_FLYE##* } ./src/te_dynamics/get_fasta.sh flye)
 
-sbatch --dependency=afterok:${FASTA_CANU##* } ./src/te_dynamics/te_sorter.sh canu
-sbatch --dependency=afterok:${FASTA_FLYE##* } ./src/te_dynamics/te_sorter.sh flye
-sbatch --dependency=afterok:${FASTA_FLYE##* } ./src/te_dynamics/te_sorter.sh ref
+SORTER_CANU=$(sbatch --dependency=afterok:${FASTA_CANU##* } ./src/te_dynamics/te_sorter.sh canu)
+SORTER_FLYE=$(sbatch --dependency=afterok:${FASTA_FLYE##* } ./src/te_dynamics/te_sorter.sh flye)
+SORTER_REF=$(sbatch ./src/te_dynamics/te_sorter.sh ref)
+
+PHYLO_REF=$(sbatch --dependency=afterok:${SORTER_REF##* } ./src/te_dynamics/phylogenetic/main.sh ref)
+PHYLO_CANU=$(sbatch --dependency=afterok:${SORTER_CANU##* }:${PHYLO_REF##*} ./src/te_dynamics/phylogenetic/main.sh canu)
+PHYLO_FLYE=$(sbatch --dependency=afterok:${SORTER_FLYE##* }:${PHYLO_REF##*} ./src/te_dynamics/phylogenetic/main.sh flye)
+
