8c9691e984ad3df543e305459e01fd5f99028b20
Axel Giottonini <axel.giottonini@gmail.com>
2022-12-16 20:17:48 +0100

    Genes Annotation QC Notebook - Genes Viewer

--- src/genes_annotation_qc/star.sh
+++ src/genes_annotation_qc/star.sh
@@ -10,6 +10,7 @@
 
 module load UHTS/Assembler/cufflinks/2.2.1;
 module add UHTS/Aligner/STAR/2.7.9a
+module load UHTS/Analysis/samtools/1.10;
 
 ASSDIR=$1
 INDIR=$( pwd )/out/genes_annotation/$ASSDIR
@@ -18,31 +19,36 @@ GENDIR=$( pwd )/out/assembly/$ASSDIR
 READS=($( pwd )/data/RNAseq/*.fastq.gz)
 
 # Convert GFF to GTF
-gffread -E $INDIR/annotated.all.gff -T -o $OUTDIR/annotated.all.gtf
+#gffread -E $INDIR/annotated.all.gff -T -o $OUTDIR/annotated.all.gtf
 
 # Generate genome index for star
-mkdir $OUTDIR/genome_index
-cd $OUTDIR/genome_index
+#mkdir $OUTDIR/genome_index
+#cd $OUTDIR/genome_index
 
-STAR \
-    --runThreadN $SLURM_CPUS_PER_TASK \
-    --runMode genomeGenerate \
-    --genomeDir $OUTDIR/genome_index \
-    --genomeFastaFiles $GENDIR/assembly.polished.fasta \
-    --sjdbGTFfile $OUTDIR/annotated.all.gtf
+#STAR \
+#    --runThreadN $SLURM_CPUS_PER_TASK \
+#    --runMode genomeGenerate \
+#    --genomeDir $OUTDIR/genome_index \
+#    --genomeFastaFiles $GENDIR/assembly.polished.fasta \
+#    --sjdbGTFfile $OUTDIR/annotated.all.gtf
 
-cd - 
+#cd - 
 
 # Map RNASeq reads
-zcat ${READS[0]} > $OUTDIR/reads.1.fasta
-zcat ${READS[1]} > $OUTDIR/reads.2.fasta
+#zcat ${READS[0]} > $OUTDIR/reads.1.fasta
+#zcat ${READS[1]} > $OUTDIR/reads.2.fasta
 
-STAR \
-    --runThreadN $SLURM_CPUS_PER_TASK \
-    --genomeDir $OUTDIR/genome_index \
-    --readFilesIn $OUTDIR/reads.1.fasta $OUTDIR/reads.2.fasta \
-    --outFileNamePrefix $OUTDIR/ \
-    --outSAMtype BAM SortedByCoordinate \
-    --outFilterMultimapNmax 10 \
-    --outFilterMismatchNoverLmax 0.01 \
-    --alignIntronMax 60000
+##STAR \
+#    --runThreadN $SLURM_CPUS_PER_TASK \
+#    --genomeDir $OUTDIR/genome_index \
+#    --readFilesIn $OUTDIR/reads.1.fasta $OUTDIR/reads.2.fasta \
+#    --outFileNamePrefix $OUTDIR/ \
+#    --outSAMtype BAM SortedByCoordinate \
+#    --outFilterMultimapNmax 10 \
+#    --outFilterMismatchNoverLmax 0.01 \
+#    --alignIntronMax 60000
+
+samtools index $OUTDIR/Aligned.sortedByCoord.out.bam -@ $SLURM_CPUS_PER_TASK
+samtools faidx $GENDIR/assembly.polished.fasta
+
+rm -rf $OUTDIR/reads.*.fasta $OUTDIR/Log.*
